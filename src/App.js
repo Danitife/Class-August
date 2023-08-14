3df6330264adaa06d53672db0567f88189f32ec7
Dev Dan <adesanyaboluwatife29@gmail.com>
2023-08-14 11:17:25 +0100

    This is the first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const dan = {
+    textDecoration: "underline",
+    color: "blue"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // VIRTUAL DOM
+    //
+    <>
+      <h1 style={dan}>Welcome to React Class</h1>
+      <h1 className='jj bg-slate-950'>Lorem ipsum dolor sit amet.</h1>
+      <h1 className='text-green-400'>Test Tailwind</h1>
+      <h1 className='bg-red-500 ml-10'></h1>
+      <h1></h1>
+      <div className='sm:flex block justify-center'>
+        <div className='w-24 h-24 m-auto sm:m-0 md:w-52 bg-orange-600 hover:bg-blue-400 transition-ease-in'></div>
+        <div className='w-24 h-24 m-auto mt-10 sm:m-0 bg-orange-300'></div>
+      </div>
+    </>
   );
 }
 
---
+++ src/First.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div>
+        <h1>Hello World</h1>
+        <img src={require("./img/sneak3.jpg")} alt="" />
+    </div>
+  )
+}
+
+export default First
+
+// function First(){
+//     return(
+//         <>
+//             <h1>Hello World</h1>
+//             <img src={require("./img/sneak3.jpg")} alt="" />
+//         </>
+//     )
+// }
+
+// export default First
